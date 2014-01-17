c3bd782eb9cba5ea089647849a961c273fa17406
Andre Schweiger <andre.spiele@yahoo.de>
2014-01-17 19:41:00 +0100

    Add ItemKill
    Signed-off-by: Andre Schweiger <andre.spiele@yahoo.de>

---
+++ SpellItems/src/me/andre111/items/item/spell/ItemKill.java
@@ -0,0 +1,43 @@
+package me.andre111.items.item.spell;
+
+import me.andre111.items.item.ItemSpell;
+
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+
+public class ItemKill extends ItemSpell {
+	private boolean self = true;
+	
+	@SuppressWarnings("unused")
+	private double range;
+	
+	@Override
+	public void setCastVar(int id, double var) {
+		if(id==0) self = var==1;
+		else if(id==1) range = var;
+	}
+	
+	@Override
+	public boolean cast(Player player, Location loc, Player target, Block block) {
+		Player pTarget = null;
+		if(self) {
+			pTarget = player;
+		} else if(target!=null) {
+			pTarget = target;
+		}
+		
+		if(pTarget!=null) {
+			castIntern(pTarget, player);
+		}
+		
+		return false;
+	}
+	
+	
+	private boolean castIntern(Player player, Player source) {
+		player.setHealth(0);
+		
+		return true;
+	}
+}
